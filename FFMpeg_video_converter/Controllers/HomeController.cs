54e987265367e8b06ef3814f799353ea21e55677
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-07-25 21:41:05 +0300

    Added basic MVC project with upload function

---
+++ FFMpeg_video_converter/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using FFMpeg_video_converter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Xabe.FFmpeg;
+
+namespace FFMpeg_video_converter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _appEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment appEnvironment)
+        {
+            _logger = logger;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult AddFile(IFormFile file)
+        {
+            if(file != null)
+            {
+                string path = Path.Combine(_appEnvironment.WebRootPath, "Files", file.FileName);
+                using (FileStream fileStream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+                return Content("File was uploaded, path: " + Path.Combine(_appEnvironment.ContentRootPath, "Files", file.FileName));
+            }
+
+            return Content("Upload error");
+        }
+
+        public async Task<IActionResult> ConvertFile()
+        {
+            return Content("No file uploaded");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FFMpeg_video_converter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FFMpeg_video_converter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
