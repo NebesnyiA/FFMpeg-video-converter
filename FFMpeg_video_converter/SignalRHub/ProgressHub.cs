cda37ef9f165438f12bc2f0742c1196aee829788
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-07-27 17:08:43 +0300

    Connected SignalR to the project

---
+++ FFMpeg_video_converter/SignalRHub/ProgressHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FFMpeg_video_converter.SignalRHub
+{
+    public class ProgressHub : Hub
+    {
+    }
+}
