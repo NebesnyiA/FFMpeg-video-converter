c4e4e1d8315983b860997337ac19884fdda1baa3
Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>
2022-07-28 22:07:53 +0300

    Added conversion info to the file model

---
+++ FFMpeg_video_converter/Models/FileModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Xabe.FFmpeg;
+using System.IO;
+
+namespace FFMpeg_video_converter.Models
+{
+    public class FileModel
+    {
+        public string fileName;
+
+        public string originalFilePath;
+        public string convertedFilePath;
+
+        public IConversion conversion;
+        public CancellationTokenSource token;
+    }
+}
