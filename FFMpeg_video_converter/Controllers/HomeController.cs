8b632dc0435c992a5479afaa771719a7c8756051
Nebesnyi Andrew <73758238+NebesnyiA@users.noreply.github.com>
2022-08-15 22:23:42 +0300

    Fixed bugs, added comments (#2)
    * Added uploaded/converted files clean functions
    
    * Updated NuGet packeges
    
    * Added remove file ajax function
    
    * Removed junk html code
    
    * Improved conversion methods, added comments
    
    * Cleaned code, added comments and regions
    
    * Added README
    
    * Added screenshots to README file
    
    Co-authored-by: Nebesnyi Andrew <73758238+artpro197@users.noreply.github.com>

--- FFMpeg_video_converter/Controllers/HomeController.cs
+++ FFMpeg_video_converter/Controllers/HomeController.cs
@@ -20,21 +20,31 @@ namespace FFMpeg_video_converter.Controllers
 {
     public class HomeController : Controller
     {
+        // request size set to 134 megabytes
+        private const int RequestSize = 1073741824;
+
         private readonly ILogger<HomeController> _logger;
         private IWebHostEnvironment _appEnvironment;
 
-        public static IHubContext<ProgressHub> hub;
-        public static CancellationTokenSource token = new CancellationTokenSource();
+        // hub provides possibility to send server answers to several clients
 
-        static Dictionary<string, FileModel> filesToConvert = new Dictionary<string, FileModel>();
+        public static IHubContext<ProgressHub> _hub;
+
+        // dictionary is used to store user's files and conversion objects
+
+        private static Dictionary<string, FileModel> _filesToConvert = new Dictionary<string, FileModel>();
 
         public HomeController(ILogger<HomeController> logger, IWebHostEnvironment appEnvironment, IHubContext<ProgressHub> hubContext)
         {
             _logger = logger;
             _appEnvironment = appEnvironment;
-            hub = hubContext;
+            _hub = hubContext;
 
             string ffmpegPass = Path.Combine(_appEnvironment.ContentRootPath, "ffmpeg");
+
+            // if statements are used to check ffmpeg binaries existence
+            // and download them if it's needed (in this case first start will be long)
+            //
 
             if (!Directory.Exists(ffmpegPass))
             {
@@ -43,32 +53,71 @@ namespace FFMpeg_video_converter.Controllers
             if (!(Directory.Exists(Path.Combine(ffmpegPass, "ffmpeg.exe"))
                 && Directory.Exists(Path.Combine(ffmpegPass, "ffprobe.exe"))))
             {
+                // downloads ffmpeg.exe and ffprobe.exe
+
                 FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, ffmpegPass).GetAwaiter().GetResult();
             }
             FFmpeg.SetExecutablesPath(ffmpegPass);
         }
+
+        #region View controllers
 
         public IActionResult Main()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Converter(string resolution, string format, List<string> file)
+        {
+            // create dir to store converted files
+
+            string outputPath = Path.Combine(_appEnvironment.WebRootPath, "ConvertedFiles");
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            // model is used to display uploaded files on converter page
+
+            ConvertPageModel model = new ConvertPageModel();
+            model.resolution = resolution;
+            model.FileList = file;
+            model.format = format;
+
+            return View(model);
+        }
+
+        #endregion
+
+        #region File management
 
         [HttpPost]
-        [RequestFormLimits(MultipartBodyLengthLimit = 1073741824)]
-        [RequestSizeLimit(1073741824)]
+        [RequestFormLimits(MultipartBodyLengthLimit = RequestSize)]
+        [RequestSizeLimit(RequestSize)]
+
+        // uploaded file to the server
+        // event listener get file upload progress
+        //
+
         public async Task<string> UploadFile(IFormFile file)
         {
             if (file != null)
             {
+                // create dir to store uploaded file
+
                 string path = Path.Combine(_appEnvironment.WebRootPath, "Files");
                 if (!Directory.Exists(path))
                 {
                     Directory.CreateDirectory(path);
                 }
 
+                // create file stream
+
                 using (FileStream stream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create))
                 {
+                    // start file uploading
+
                     await file.CopyToAsync(stream);
                     stream.Flush();
                 }
@@ -78,58 +127,97 @@ namespace FFMpeg_video_converter.Controllers
             return "Upload_error";
         }
 
-        [HttpPost]
-        public IActionResult Converter(string resolution, string format, List<string> file)
+        // function is called, when user presses remove button in amin form
+        // also called when user leaves web-site
+        //
+
+        public void RemoveUploadedFile(string fileName)
         {
-            string filePath = Path.Combine(_appEnvironment.WebRootPath, "Files");
-            string outputPath = Path.Combine(_appEnvironment.WebRootPath, "ConvertedFiles");
-            if (!Directory.Exists(outputPath))
+            string filePath = Path.Combine(_appEnvironment.WebRootPath, "Files", fileName);
+
+            if (System.IO.File.Exists(filePath))
             {
-                Directory.CreateDirectory(outputPath);
+                System.IO.File.Delete(filePath);
             }
-
-            ConvertPageModel model = new ConvertPageModel();
-
-            model.resolution = resolution;
-            model.FileList = file;
-            model.format = format;
-
-            return View(model);
         }
+
+        // removes conversion result when user leaves
+        // or stops convert process
+        //
+
+        public void RemoveConvertedFile(string fileName, string format)
+        {
+            string filePath = Path.Combine(_appEnvironment.WebRootPath, "ConvertedFiles", Path.ChangeExtension(fileName, format));
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+            RemoveUploadedFile(fileName);
+        }
+
+        #endregion
+
+        #region Conversion management
+
+        // is called for every uploaded file when user opens converter page
+        //
 
         public async Task ConvertFile(string fileName, string connectionId, string format, string resolution)
         {
+            // vars are used to create conversion object
+
             string convertedFileName = Path.ChangeExtension(fileName, format);
             string inputFile = Path.Combine(_appEnvironment.WebRootPath, "Files", fileName);
             string outputFile = Path.Combine(_appEnvironment.WebRootPath, "ConvertedFiles", convertedFileName);
 
+            // conversion object is created
+
             ConvertClass convertProcesses = new ConvertClass(inputFile, outputFile, format, resolution);
 
-            filesToConvert.Add(fileName, convertProcesses.GetFileObject());
-            filesToConvert[fileName].fileName = fileName;
+            // and saved to the dictionary
 
-            filesToConvert[fileName].conversion.OnProgress += (sender, args) =>
+            _filesToConvert.Add(fileName, convertProcesses.GetFileObject());
+            _filesToConvert[fileName].fileName = fileName;
+
+            // created conversion progress sender
+
+            _filesToConvert[fileName].conversion.OnProgress += (sender, args) =>
             {
+                // hub sends progress and file info to the cliend
+                // every second
+
                 var percent = (int)(Math.Round(args.Duration.TotalSeconds / args.TotalLength.TotalSeconds, 2) * 100);
-                hub.Clients.Client(connectionId).SendAsync("Progress", percent, fileName, convertedFileName);
-                Debug.WriteLine(percent);
+                _hub.Clients.Client(connectionId).SendAsync("Progress", percent, fileName, convertedFileName);
             };
 
-            await filesToConvert[fileName].conversion.Start(filesToConvert[fileName].token.Token);
+            // start conversion, set token to control it
+
+            await _filesToConvert[fileName].conversion.Start(_filesToConvert[fileName].token.Token);
         }
+
+        // function removes conversion from the dictionary
+        //
 
         public void RemoveConversion(string fileName)
         {
-            filesToConvert.Remove(fileName);
+            _filesToConvert.Remove(fileName);
         }
+
+        // function is called, when user presses stop button
+        //
 
         public string CancelConversion(string fileName)
         {
-            filesToConvert[fileName].token.Cancel();
+            // use saved token to stop connected to it conversion
+
+            _filesToConvert[fileName].token.Cancel();
             RemoveConversion(fileName);
 
             return "Stoped conversion";
         }
+
+        #endregion
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ README.md
@@ -0,0 +1,31 @@
+﻿# FFMpeg video converter
+
+FFMpeg video converter is a web-appliction, which is an interface for ffmpeg open-source library.
+Uses Xabe.FFMpeg framework to convert video files of each format and different size to .AVI, .MOV, .MP4.
+Also provides possibility to change video resolution.
+
+![изображение](https://user-images.githubusercontent.com/73758238/184699738-c11e56b6-c504-470f-8281-43ad7cf248c0.png)
+
+## Functionality
+
+>Current version of this project implements this functions:
+>
+>`Uploading and processing of large files`
+>
+>`Conversion to AVI, MP4, MOV formats`
+>
+>`Resolution changing`
+>
+>`Conversion of several files`
+>
+
+## Usage instructions
+
+Interface is simple. All is needed to drop several files, choose parameters and press "Convert".
+
+![изображение](https://user-images.githubusercontent.com/73758238/184698826-5dc91f30-069d-42a6-907e-14787eeb1151.png)
+
+As result, conversion will be started. Possibility to cancel the process exists.
+When conversion finished, download button would appear.
+
+![изображение](https://user-images.githubusercontent.com/73758238/184699120-39a4b0d3-096b-49ef-98d3-0c28c7bac512.png)
